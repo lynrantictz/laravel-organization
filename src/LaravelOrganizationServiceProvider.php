14ae3c240165de16b4c667bd22e61c8afdfd1b39
Hamis Juma <hamisjuma1@gmail.com>
2022-07-01 22:40:38 +0300

    Inital Commit

---
+++ src/LaravelOrganizationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lynrantictz\LaravelOrganization;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelOrganizationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
