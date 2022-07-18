89f7bc1cbee85f98537ed83df55c5eb42540bc36
Hamis Juma <hamisjuma1@gmail.com>
2022-07-18 15:26:05 +0300

    Complete og:init command

--- src/Services/Generator.php
+++ src/Services/Generator.php
@@ -6,38 +6,40 @@ use Illuminate\Support\Facades\File;
 
 trait Generator
 {
-    /**
-     * Generate Folders for Models and Repository
-     */
+
     public function initiate()
+    {
+        $this->createModelsFolder();
+        $this->publishBaseModel();
+        $this->createRepositoriesFolder();
+        $this->publishBaseRepository();
+    }
+
+    public function createModelsFolder()
     {
         if (!file_exists(app_path('Models'))) {
             File::makeDirectory(app_path('Models'), 0777, true, true);
         }
+    }
 
-        if (!file_exists(app_path('Models/BaseModel.php'))) {
-            $this->publishBaseModel();
-        }
-
+    public function createRepositoriesFolder()
+    {
         if (!file_exists(app_path('Repositories'))) {
             File::makeDirectory(app_path('Repositories'), 0777, true, true);
         }
-
-        if (!file_exists(app_path('Repositories/BaseRepository.php'))) {
-            $this->publishBaseRepository();
-        }
-
     }
 
     private function publishBaseModel()
     {
-        File::copy(__DIR__.'/../Templates/BaseModel.php', app_path('Models/BaseModel.php'));
+        if (!file_exists(app_path('Models/BaseModel.php'))) {
+            File::copy(__DIR__ . '/../Templates/BaseModel.php', app_path('Models/BaseModel.php'));
+        }
     }
 
     private function publishBaseRepository()
     {
-        File::copy(__DIR__.'/../Templates/BaseRepository.php', app_path('Repositories/BaseRepository.php'));
+        if (!file_exists(app_path('Repositories/BaseRepository.php'))) {
+            File::copy(__DIR__ . '/../Templates/BaseRepository.php', app_path('Repositories/BaseRepository.php'));
+        }
     }
-
-
 }
